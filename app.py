4df2424b960a2aad31c4e1d2fdb9a05b121fd813
Avinash Kumar Patel <88762617+Avipatel129@users.noreply.github.com>
2023-05-16 16:10:35 +0530

    Update app.py
    change of app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template, request
+import re
+import warnings
 
-from chat_bot import getInfo
+import numpy as np
+import pandas as pd
+import pyttsx3
+from flask import Flask, render_template, request
+from sklearn import preprocessing
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier, _tree
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 app = Flask(__name__)
 
-@app.get("/")
-def index_get():
-    return render_template("base.html")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Add the code for symptom prediction and disease information here
+    # Retrieve the user-entered symptoms from the form
+    symptoms = request.form.get('symptoms')
+    # Perform the necessary prediction and display the results
+
+    return render_template('result.html', symptoms=symptoms, predicted_disease=predicted_disease)
